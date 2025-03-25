f08fc008046b635345552b12fb64276e2d6dfe9b
Tagir-Alisheykhov <ahitahir@yandex.ru>
2025-03-25 13:07:27 +0300

    Create styles and render

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CotalogConfig(AppConfig):
+class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cotalog'
+    name = 'catalog'
